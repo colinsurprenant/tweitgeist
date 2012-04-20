3910af2a06cce3f0023e2bb8e971eaca1c4d8588
Colin Surprenant <colin.surprenant@gmail.com>
2012-04-20 08:23:34 -0400

    storing historical ranks

--- lib/tweitgeist/storm/merge_bolt.rb
+++ lib/tweitgeist/storm/merge_bolt.rb
@@ -6,10 +6,11 @@ module Tweitgeist
 
     TOP_N = 10
     PUSH_INTERVAL = 5
+    HISTORY_SIZE = (24*60*60)/PUSH_INTERVAL # 24h
 
     on_init do
       @rankings = Hash.new
-      @last = Time.now.to_i
+      @last_time = Time.now.to_i
       @redis = Redis.new(:host => "localhost", :port => 6379)
     end
 
@@ -21,11 +22,19 @@ module Tweitgeist
       sorted = @rankings.sort{|a, b| b[1] <=> a[1]} # decreasing order on count
       @rankings.delete(sorted.pop[0]) while sorted.size > TOP_N 
 
-      # poor's man delayed push, ok since we receive infrequent tuples
-      if (now = Time.now.to_i) > (@last + PUSH_INTERVAL)
-        @last = now
-        @redis.rpush("rankings", sorted.to_json) unless sorted.empty? 
+      # poor's man delayed push, ok since we receive 'infrequent' tuples
+      if (now = Time.now.to_i) > (@last_time + PUSH_INTERVAL)
+        @last_time = now
+        unless sorted.empty?
+          @redis.pipelined do
+            @redis.rpush('rankings', sorted.to_json)
+
+            @redis.lpush('past_rankings', {'created_at' => now, 'rankings' => sorted}.to_json)
+            @redis.ltrim('past_rankings', 0, HISTORY_SIZE - 1)
+          end
+        end
       end
     end
+
   end
 end
